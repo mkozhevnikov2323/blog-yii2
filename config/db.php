acbc26863bac5223c35fe31d478bd78f8c66b4cd
maksim <mkozhevnikov23@gmail.com>
2023-01-16 23:02:25 +0400

    create connection to DB

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=' . HOSTNAME . ';dbname=' . DB_NAME,
+    'username' => DB_USERNAME,
+    'password' => DB_PASSWORD,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
